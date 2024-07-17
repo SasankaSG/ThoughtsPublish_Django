dbb2f18e7ae7823e459bd241cf1d97f678ced2a1
SasankaSG <161485283+SasankaSG@users.noreply.github.com>
2024-07-17 17:25:16 +0530

    removed creds settings.py

--- edenthought/edenthought/settings.py
+++ edenthought/edenthought/settings.py
@@ -149,7 +149,7 @@ EMAIL_USE_TLS = 'True'
 EMAIL_HOST_USER = 'sasankasi@gmail.com'
 EMAIL_HOST_PASSWORD = '' #google created password
 
-DEFAULT_FROM_EMAIL = 'sasankasrungarakavi@gmail.com'
+DEFAULT_FROM_EMAIL = 'sasankasi@gmail.com'
 
 
 
